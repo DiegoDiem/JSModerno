f97fa65c7072416265d2559cce63beec1b0930d6
DiegoDiem <ldmg.9@hotmail.com>
2023-05-19 19:31:41 -0600

    Revert "cambio 1"
    This reverts commit b9062f6c2a20c7cafc4fd321489003610295a86e.

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -3,4 +3,4 @@
 const nombre = prompt('Cual es tu nombre?');
 // Toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript`;
-// Commentx
+// Comment
